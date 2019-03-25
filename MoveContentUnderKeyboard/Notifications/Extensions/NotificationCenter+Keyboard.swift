daca02ff07f56cf7842ace7f760c2826f7568efc
wesleysadler <wesley_sadler@yahoo.com>
2019-03-25 10:29:08 -0500

    Change copyright and fix error in test project

--- MoveContentUnderKeyboard/Notifications/Extensions/NotificationCenter+Keyboard.swift
+++ MoveContentUnderKeyboard/Notifications/Extensions/NotificationCenter+Keyboard.swift
@@ -3,8 +3,28 @@
 //  MoveContentUnderKeyboard
 //
 //  Created by wesley a sadler on 3/11/19.
-//  Copyright © 2019 Digital Sadler. All rights reserved.
 //
+//  The MIT License
+//
+//  Copyright © 2019 Digital Sadler.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  opies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import Foundation
 
@@ -18,22 +38,3 @@ extension NotificationCenter {
          return NotificationToken(token: token, center: self)
     }
 }
-
-/*
- extension NotificationCenter {
- func addObserver<A>(forDescriptor d: NotificationDescriptor<A>, using block: @escaping (A) -> ()) -> Token {
- let t = addObserver(forName: d.name, object: nil, queue: nil, using: { note in
- block(d.convert(note))
- })
- return Token(token: t, center: self)
- }
- }
- 
- var token: Token? = center.addObserver(forDescriptor: playgroundNotification, using: { print($0) })
- 
- // ...
- 
- token = nil
- 
- 
-*/
--- MoveContentUnderKeyboardTests/MoveContentUnderKeyboardTests.swift
+++ MoveContentUnderKeyboardTests/MoveContentUnderKeyboardTests.swift
@@ -3,8 +3,28 @@
 //  MoveContentUnderKeyboardTests
 //
 //  Created by Wesley Sadler on 2/25/16.
-//  Copyright © 2016 Digital Sadler. All rights reserved.
 //
+//  The MIT License
+//
+//  Copyright © 2016 - 2019 Digital Sadler.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  opies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import XCTest
 @testable import MoveContentUnderKeyboard
@@ -28,7 +48,7 @@ class MoveContentUnderKeyboardTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
